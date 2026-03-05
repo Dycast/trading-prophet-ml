03ae01c55875fa88242b209e7d9cda81e4df2f82
Dycast <acksolutions@gmail.com>
2026-03-05 06:16:51 -0800

    Add MIT License file
    Added MIT License to the project.

---
+++ new.py
@@ -0,0 +1,59 @@
+
+Skip to content
+
+    Dycast
+    trading-prophet-ml
+
+Repository navigation
+
+    Code
+    Pull requests
+    Actions
+    Projects
+    Wiki
+    Security
+    Insights
+    Settings
+
+Commit 82caeee
+zahid-marwat
+zahid-marwat
+authored
+3 weeks ago
+Initial commit
+main
+
+0 parents  commit 82caeee
+
+1 file changed
++21
+-0
+lines changed
+ 
+‎LICENSE‎
++21Lines changed: 21 additions & 0 deletions
+Original file line number	Original file line	Diff line number	Diff line change
+@@ -0,0 +1,21 @@
+MIT License
+Copyright (c) 2026 Zahid Ullah Khan
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+0 commit comments
+Comments
+0 (0)
+
+You're not receiving notifications from this thread.
+R2 to R7 selected.
